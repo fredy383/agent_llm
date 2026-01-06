61b8f9bd2b9aac3a8d8c50800868adb5dc1e407a
fredy383 <frederic.didelon@outlook.com>
2026-01-06 21:24:37 +0100

    Update: CH2 L5 en cours

---
+++ functions/run_python_file.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+
+    abs_path = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_path, file_path))
+    print(target_dir)
+
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
+
+    if not valid_target_dir:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(target_dir):
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+    
+    if not file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file'
+    
+    command = ["python", target_dir]
+    if args != None:
+        command.extend(args)
+
+    result = subprocess.run(command,cwd=abs_path,stdout=True,stderr=True,text=True,timeout=30)
+
+    error_code = result.returncode
+    stdout = result.stdout
+    stderr = result.stderr
+    output = ""
+    output2 =""
+
+    if error_code != 0:
+        return f"Process exited with code {error_code}"
+
+    if not stdout and not stderr:
+        return "No output produced"
+    
+    if stdout:
+        output = f"STDOUT: {stdout}"
+
+    if stderr:
+        output2 = f"STDERR: {stderr}"
+
+    return output + output2
---
+++ test_run_python_file.py
@@ -0,0 +1,3 @@
+from functions.run_python_file import *
+
+print(run_python_file("calculator", "main.py"))
