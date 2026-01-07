6d1dfce9fd0f29d126d859bd66f9d9d0f8874942
fredy383 <frederic.didelon@outlook.com>
2026-01-07 21:23:11 +0100

    Update: CH2 L5 DONE

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -2,45 +2,52 @@ import os
 import subprocess
 
 def run_python_file(working_directory, file_path, args=None):
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_path, file_path))
 
-    abs_path = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(abs_path, file_path))
-    print(target_dir)
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
 
-    # Will be True or False
-    valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
+        if not valid_target_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(target_dir):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        
+        if not file_path.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file'
+        
+        command = ["python", target_dir]
+        if args:
+            command.extend(args)
 
-    if not valid_target_dir:
-        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.isfile(target_dir):
-        return f'Error: "{file_path}" does not exist or is not a regular file'
-    
-    if not file_path.endswith('.py'):
-        return f'Error: "{file_path}" is not a Python file'
-    
-    command = ["python", target_dir]
-    if args != None:
-        command.extend(args)
+        result = subprocess.run(
+            command,
+            cwd=abs_path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            timeout=30)
 
-    result = subprocess.run(command,cwd=abs_path,stdout=True,stderr=True,text=True,timeout=30)
+        error_code = result.returncode
+        stdout = result.stdout
+        stderr = result.stderr
+        output = ""
+        output2 =""
 
-    error_code = result.returncode
-    stdout = result.stdout
-    stderr = result.stderr
-    output = ""
-    output2 =""
+        if error_code != 0:
+            return f"Process exited with code {error_code}"
 
-    if error_code != 0:
-        return f"Process exited with code {error_code}"
+        if not stdout and not stderr:
+            return "No output produced"
+        
+        if stdout:
+            output = f"STDOUT: {stdout}"
 
-    if not stdout and not stderr:
-        return "No output produced"
-    
-    if stdout:
-        output = f"STDOUT: {stdout}"
+        if stderr:
+            output2 = f"STDERR: {stderr}"
 
-    if stderr:
-        output2 = f"STDERR: {stderr}"
-
-    return output + output2
+        return output + output2
+    except Exception as e:
+        return f"Error : executing Python file: {e}"
